1058e0582f25d832e440ed53460cdb0bc956c037
Santiago <santiagocortondo25@gmail.com>
2023-09-06 20:55:15 -0300

    s

--- buscador_de_img_nasa/js/space.js
+++ buscador_de_img_nasa/js/space.js
@@ -2,8 +2,9 @@ const boton = document.getElementById("btnBuscar");
 const contenedor = document.getElementById("contenedor");
 
 boton.addEventListener("click", function () {
-  const planeta = document.getElementById("inputBuscar").value;
-  var url = `https://images-api.nasa.gov/search?q=${planeta}`;
+  const planeta = document.getElementById("inputBuscar").value; // Mover esta línea aquí
+  
+  const url = `https://images-api.nasa.gov/search?q=${planeta}`;
   
   fetch(url)
     .then((res) => res.json())
@@ -17,20 +18,26 @@ boton.addEventListener("click", function () {
 
 function mostrarDatos(data) {
   const items = data.collection.items;
-  contenedor.innerHTML = "";
+  contenedor.innerHTML = ""; // Limpiar el contenedor antes de agregar nuevas tarjetas
+  
   items.forEach((item) => {
-    const imgSrc = item.links[0].href;
-    const imgTitle = item.data[0].title;
-    const img = document.createElement("img");
-    img.src = imgSrc;
-    img.alt = imgTitle;
-    const title = document.createElement("h2");
-    title.textContent = imgTitle;
-    contenedor.appendChild(img);
-    contenedor.appendChild(title);
+    const title = item.data[0].title;
+    const description = item.data[0].desc;
+    const dateCreated = item.data[0].dateCreated; // Asumiendo que tienes esta propiedad en tu objeto 'data'
+    const imageUrl = item.links[0].href;
+
+    const cardElement = document.createElement("div");
+    cardElement.className = "col-lg-4 col-md-6 mb-4";
+    cardElement.innerHTML = `
+      <div class="card">
+        <img src="${imageUrl}" class="card-img-top" alt="${title}">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${description}</p>
+          <p class="card-text">Fecha de Creación: ${dateCreated}</p>
+        </div>
+      </div>`;
+
+    contenedor.appendChild(cardElement);
   });
 }
-
-        
-        
-
