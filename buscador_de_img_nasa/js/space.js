69526d385b2ef25b3f8908c1edbf4fb217eb9599
Santiago <santiagocortondo25@gmail.com>
2023-09-06 21:03:46 -0300

    s

--- buscador_de_img_nasa/js/space.js
+++ buscador_de_img_nasa/js/space.js
@@ -1,43 +1,50 @@
-const boton = document.getElementById("btnBuscar");
-const contenedor = document.getElementById("contenedor");
+const searchButton = document.getElementById("btnBuscar");
+const resultContainer = document.getElementById("contenedor");
 
-boton.addEventListener("click", function () {
-  const planeta = document.getElementById("inputBuscar").value; 
-  
-  const url = `https://images-api.nasa.gov/search?q=${planeta}`;
-  
-  fetch(url)
-    .then((res) => res.json())
+searchButton.addEventListener("click", () => {
+  const searchTerm = document.getElementById("inputBuscar").value;
+
+  fetchNASAData(searchTerm)
     .then((data) => {
-      mostrarDatos(data);
+      displayData(data);
     })
     .catch((error) => {
       console.error("Error al cargar los datos:", error);
     });
 });
 
-function mostrarDatos(data) {
+function fetchNASAData(searchTerm) {
+  const url = `https://images-api.nasa.gov/search?q=${searchTerm}`;
+  return fetch(url).then((response) => response.json());
+}
+
+function displayData(data) {
   const items = data.collection.items;
-  contenedor.innerHTML = ""; 
-  
+  resultContainer.innerHTML = "";
+
   items.forEach((item) => {
     const title = item.data[0].title;
-    const description = item.data[0].desc;
-    const dateCreated = item.data[0].dateCreated; 
+    const description = item.data[0].description;
+    const dateCreated = item.data[0].date_created;
     const imageUrl = item.links[0].href;
 
-    const cardElement = document.createElement("div");
-    cardElement.className = "col-lg-4 col-md-6 mb-4";
-    cardElement.innerHTML = `
-      <div class="card">
-        <img src="${imageUrl}" class="card-img-top" alt="${title}">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text">${description}</p>
-          <p class="card-text">Fecha de Creación: ${dateCreated}</p>
-        </div>
-      </div>`;
-
-    contenedor.appendChild(cardElement);
+    const cardElement = createCard(title, description, dateCreated, imageUrl);
+    resultContainer.appendChild(cardElement);
   });
+}
+
+function createCard(title, description, dateCreated, imageUrl) {
+  const cardElement = document.createElement("div");
+  cardElement.className = "col-lg-4 col-md-6 mb-4";
+  cardElement.innerHTML = `
+    <div class="card">
+      <img src="${imageUrl}" class="card-img-top" alt="${title}">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${description}</p>
+        <p class="card-text">Fecha de Creación: ${dateCreated}</p>
+      </div>
+    </div>`;
+
+  return cardElement;
 }
